e3335330c3e7fbfec69d6e02e0a9ff13a505add2
Gyanendra Prakash <prakash.gyanendra5@gmail.com>
2021-05-29 08:30:55 +0530

    front end added, auto scroll added, and others

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,102 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput =$messageForm.querySelector('input')
+const $messageFormButton =$messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
+
+//templates
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+
+// options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight > scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 socket.on('message',(message)=>{
 
     console.log(message)
+    const html = Mustache.render(messageTemplate,{
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
 
 })
+
+socket.on('locationMessage',(message)=>{
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate,{
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
+
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
 
 
 document.querySelector('#message-form').addEventListener('submit', (e)=>{
 
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled','disabled')
+
+
+
+
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message,(error)=>{
-        
-        return console.log(error)
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
     })
 })
 
@@ -29,23 +109,27 @@ document.querySelector('#send-location').addEventListener('click', ()=>
 
     }
    
-
+    $sendLocationButton.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position)=>{
 
-        //console.log(position)
+        
+
+        console.log(position)
         socket.emit('sendLocation', {
             
             latitude : position.coords.latitude,
             longitude: position.coords.longitude
 
 
-        },(acknowledge)=>{
+        },()=>{
+                 console.log("randi")
+            $sendLocationButton.removeAttribute('disabled')
 
-            console.log(acknowledge)
+            console.log("locations shared")
         })
 
 
-
+        
 
         
     })
@@ -55,4 +139,9 @@ document.querySelector('#send-location').addEventListener('click', ()=>
 
 
 )
-
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
